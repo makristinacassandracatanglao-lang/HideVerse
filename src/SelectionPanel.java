cc12783e9b7c8a6494cd62ec67d889ea9a1c57a8
min-stradamus <elladurante5@gmail.com>
2025-11-28 15:17:09 +0800

    Update SelectionPanel.java

--- src/SelectionPanel.java
+++ src/SelectionPanel.java
@@ -2,49 +2,60 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SelectionPanel extends JPanel{
-    public SelectionPanel(ActionListener listener){
+public class SelectionPanel extends JPanel {
+    public SelectionPanel(ActionListener listener) {
         setLayout(new BorderLayout());
-        setBackground(Color.WHITE);
+        Color TEAL = new Color(9, 33, 60);
+        setBackground(TEAL);
 
         ImageIcon logo = new ImageIcon("assets/title.gif");
         JLabel logoJLB = new JLabel(logo, SwingConstants.CENTER);
-        add(logoJLB, BorderLayout.NORTH);
+        logoJLB.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         JPanel centerPanel = new JPanel(new GridLayout(0, 1, 10, 10));
+        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
         centerPanel.setOpaque(false);
 
         ImageIcon levels = new ImageIcon("assets/levels.png");
         JLabel levJLB = new JLabel(levels, SwingConstants.CENTER);
-        centerPanel.add(levJLB);
-        
+        levJLB.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         JButton easyButton = new JButton(new ImageIcon("assets/easy.png"));
         JButton medButton = new JButton(new ImageIcon("assets/medium.png"));
         JButton hardButton = new JButton(new ImageIcon("assets/hard.png"));
 
+        easyButton.setActionCommand("EASY");
+        easyButton.addActionListener(listener);
+        easyButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         easyButton.setBorderPainted(false);
         easyButton.setContentAreaFilled(false);
         easyButton.setFocusPainted(false);
 
+        medButton.setActionCommand("MEDIUM");
+        medButton.addActionListener(listener);
+        medButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         medButton.setBorderPainted(false);
         medButton.setContentAreaFilled(false);
         medButton.setFocusPainted(false);
 
+        hardButton.setActionCommand("HARD");
+        hardButton.addActionListener(listener);
+        hardButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         hardButton.setBorderPainted(false);
         hardButton.setContentAreaFilled(false);
         hardButton.setFocusPainted(false);
 
-        easyButton.setActionCommand("EASY");
-        medButton.setActionCommand("MEDIUM");
-        hardButton.setActionCommand("HARD");
-
-        easyButton.addActionListener(listener);
-        medButton.addActionListener(listener);
-        hardButton.addActionListener(listener);
-
+        centerPanel.add(Box.createVerticalStrut(60));
+        centerPanel.add(logoJLB);
+        centerPanel.add(Box.createVerticalStrut(40));
+        centerPanel.add(levJLB);
+        centerPanel.add(Box.createVerticalStrut(25));
         centerPanel.add(easyButton);
+        centerPanel.add(Box.createVerticalStrut(15));
         centerPanel.add(medButton);
+        centerPanel.add(Box.createVerticalStrut(15));
         centerPanel.add(hardButton);
+        centerPanel.add(Box.createVerticalGlue());
 
         add(centerPanel, BorderLayout.CENTER);
     }
